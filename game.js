fed4e40f8c6c8427a6072523b88d2519051d3b0e
Jullia <juliasswork@gmail.com>
2026-05-24 12:48:53 +0200

    Add pixel art sprites for player, enemy, coins and platforms

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const config = {
   type: Phaser.AUTO,
   width: 640,
   height: 360,
+  backgroundColor: '#000000',
   physics: {
     default: 'arcade',
     arcade: { gravity: { y: 600 }, debug: false }
@@ -14,46 +15,107 @@ const game = new Phaser.Game(config);
 function preload() {}
 
 function create() {
-   // Фон со звёздами
-    const bg = this.add.graphics();
-    bg.fillStyle(0x1a1a2e);
-    bg.fillRect(0, 0, 640, 360);
 
-    for (let i = 0; i < 80; i++) {
+  // Фон со звёздами
+  const bg = this.add.graphics();
+  bg.fillStyle(0x1a1a2e);
+  bg.fillRect(0, 0, 640, 360);
+  for (let i = 0; i < 80; i++) {
     const x = Phaser.Math.Between(0, 640);
     const y = Phaser.Math.Between(0, 360);
     const size = Phaser.Math.FloatBetween(0.5, 2);
     const brightness = Phaser.Math.Between(150, 255);
-    const color = Phaser.Display.Color.GetColor(brightness, brightness, brightness);
-    bg.fillStyle(color);
+    bg.fillStyle(Phaser.Display.Color.GetColor(brightness, brightness, brightness));
     bg.fillRect(x, y, size, size);
-    }
-  
+  }
+
+  // Текстура платформы
+  const tg = this.make.graphics({ x: 0, y: 0, add: false });
+  tg.fillStyle(0x8B4513);
+  tg.fillRect(0, 0, 32, 16);
+  tg.fillStyle(0x228B22);
+  tg.fillRect(0, 0, 32, 5);
+  tg.fillStyle(0x33aa33);
+  tg.fillRect(2, 1, 4, 3);
+  tg.fillRect(10, 0, 3, 4);
+  tg.fillRect(20, 1, 5, 3);
+  tg.fillStyle(0x000000);
+  tg.fillRect(0, 5, 32, 1);
+  tg.generateTexture('tile', 32, 16);
+  tg.destroy();
+
+  // Текстура игрока
+  const pg = this.make.graphics({ x: 0, y: 0, add: false });
+  const playerPixels = [
+    [0,0,1,1,1,1,0,0],
+    [0,1,1,1,1,1,1,0],
+    [0,2,2,3,3,2,2,0],
+    [0,2,3,3,3,3,2,0],
+    [0,0,3,3,3,3,0,0],
+    [0,4,4,4,4,4,4,0],
+    [4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4],
+    [0,4,4,0,0,4,4,0],
+    [0,5,5,0,0,5,5,0],
+    [0,5,5,0,0,5,5,0],
+    [0,6,6,0,0,6,6,0],
+  ];
+  const pColors = [0, 0xff0000, 0xffcc99, 0x000000, 0x0044ff, 0x8B4513, 0x8B6914];
+  playerPixels.forEach((row, y) => {
+    row.forEach((val, x) => {
+      if (val > 0) {
+        pg.fillStyle(pColors[val]);
+        pg.fillRect(x * 2, y * 2, 2, 2);
+      }
+    });
+  });
+  pg.generateTexture('player', 16, 24);
+  pg.destroy();
+
+  // Текстура врага — слайм
+  const eg = this.make.graphics({ x: 0, y: 0, add: false });
+  const enemyPixels = [
+    [0,0,0,1,1,1,1,0],
+    [0,0,1,1,1,1,1,1],
+    [0,1,1,2,1,1,2,1],
+    [0,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1],
+    [0,1,3,1,1,3,1,0],
+    [0,0,1,1,1,1,0,0],
+  ];
+  const eColors = [0, 0xff3333, 0xffffff, 0x000000];
+  enemyPixels.forEach((row, y) => {
+    row.forEach((val, x) => {
+      if (val > 0) {
+        eg.fillStyle(eColors[val]);
+        eg.fillRect(x * 2, y * 2, 2, 2);
+      }
+    });
+  });
+  eg.generateTexture('enemy', 16, 16);
+  eg.destroy();
+
+  // Текстура монеты
+  const cg = this.make.graphics({ x: 0, y: 0, add: false });
+  cg.fillStyle(0xffdd00);
+  cg.fillCircle(6, 6, 6);
+  cg.fillStyle(0xffaa00);
+  cg.fillCircle(6, 6, 4);
+  cg.fillStyle(0xffff88);
+  cg.fillRect(4, 3, 2, 2);
+  cg.generateTexture('coin', 12, 12);
+  cg.destroy();
+
+  // Платформы
   const platforms = this.physics.add.staticGroup();
-
-  // Пол
-  platforms.create(320, 350, null)
-    .setDisplaySize(640, 20).setTint(0x00ff88).refreshBody();
-
-  // Потолок
-  platforms.create(320, 10, null)
-    .setDisplaySize(640, 20).setTint(0x00ff88).refreshBody();
-
-  // Платформы в воздухе
-  platforms.create(150, 250, null)
-    .setDisplaySize(120, 16).setTint(0x4488ff).refreshBody();
-
-  platforms.create(350, 180, null)
-    .setDisplaySize(120, 16).setTint(0x4488ff).refreshBody();
-
-  platforms.create(530, 250, null)
-    .setDisplaySize(120, 16).setTint(0x4488ff).refreshBody();
-
-  platforms.create(250, 110, null)
-    .setDisplaySize(120, 16).setTint(0xff4488).refreshBody();
-
-  platforms.create(470, 110, null)
-    .setDisplaySize(120, 16).setTint(0xff4488).refreshBody();
+  platforms.create(320, 350, 'tile').setDisplaySize(640, 20).refreshBody();
+  platforms.create(320, 10,  'tile').setDisplaySize(640, 20).refreshBody();
+  platforms.create(150, 250, 'tile').setDisplaySize(120, 16).refreshBody();
+  platforms.create(350, 180, 'tile').setDisplaySize(120, 16).refreshBody();
+  platforms.create(530, 250, 'tile').setDisplaySize(120, 16).refreshBody();
+  platforms.create(250, 110, 'tile').setDisplaySize(120, 16).refreshBody();
+  platforms.create(470, 110, 'tile').setDisplaySize(120, 16).refreshBody();
 
   // Монеты
   this.coins = this.physics.add.staticGroup();
@@ -62,157 +124,144 @@ function create() {
     [250, 80],  [470, 80],  [400, 320]
   ];
   coinPositions.forEach(([x, y]) => {
-    this.coins.create(x, y, null)
-      .setDisplaySize(12, 12).setTint(0xffdd00).refreshBody();
+    this.coins.create(x, y, 'coin').refreshBody();
   });
 
   // Игрок
-    const playerGraphics = this.make.graphics({ x: 0, y: 0, add: false });
-    playerGraphics.fillStyle(0xff6600);
-    playerGraphics.fillRect(0, 0, 28, 28);
-    playerGraphics.generateTexture('player', 28, 28);
-    playerGraphics.destroy();
+  this.player = this.physics.add.sprite(60, 300, 'player');
+  this.player.setBounce(0).setCollideWorldBounds(true);
+  this.physics.add.collider(this.player, platforms);
 
-    this.player = this.physics.add.sprite(60, 300, 'player');
-    this.player.setBounce(0).setCollideWorldBounds(true);
-    this.player.setBounce(0).setCollideWorldBounds(true);
+  // Враг
+  const enemyGfx = this.make.graphics({ x: 0, y: 0, add: false });
+  this.enemy = this.physics.add.sprite(400, 300, 'enemy');
+  this.enemy.setCollideWorldBounds(true).setBounce(1, 0);
+  this.enemy.setVelocityX(120);
+  this.physics.add.collider(this.enemy, platforms);
 
-    this.physics.add.collider(this.player, platforms);
+  // Жизни
+  this.lives = 3;
+  this.livesText = this.add.text(550, 10, '❤️ x3', {
+    fontSize: '14px', fill: '#ff4444'
+  }).setScrollFactor(0);
 
-  // Сбор монет
+  // Счёт
   this.score = 0;
-  this.physics.add.overlap(this.player, this.coins, (player, coin) => {
-    coin.destroy();
-    this.score++;
-    this.scoreText.setText('Монеты: ' + this.score);
-    new Audio(this.soundCoin).play();
-  });
-
-  // Управление
-    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
-    this.keyX = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-    this.shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
-    this.gravityFlipped = false;
-
-  // Интерфейс
   this.scoreText = this.add.text(10, 10, 'Монеты: 0', {
     fontSize: '14px', fill: '#ffdd00'
   }).setScrollFactor(0);
 
-  this.add.text(10, 340, 'S D движение  |  E прыжок вверх  |  X прыжок вниз  |  SHIFT гравитация', {
-    fontSize: '10px', fill: '#aaaaaa'
-  }).setScrollFactor(0);
-   
-  //Звуки
-    this.soundJump = jsfxr([0,0.35,0.12,,0.18,0.28,,,,,,,,,,,,,1,,,,,0.5]);
-    this.soundFlip = jsfxr([3,0.05,0.25,,0.1,0.15,,,,,,,,,,,-0.12,,1,,,,,0.5]);
-    this.soundCoin = jsfxr([0,,,0.02,0.1,0.6,,,0.54,,,0.36,,,,,,,,1,,,,,0.5]);
-  
-    // Музыка
-    // Фоновая 8-битная музыка
-    const notes = [
-    329, 329, 0, 329, 0, 262, 329, 0,
-    392, 0, 0, 0, 196, 0, 0, 0
-    ];
+  // Сбор монет
+  this.physics.add.overlap(this.player, this.coins, (player, coin) => {
+    coin.destroy();
+    this.score++;
+    this.scoreText.setText('Монеты: ' + this.score);
+    this.playSound(500, 900, 0.1);
+  });
 
-    let noteIndex = 0;
-    const bpm = 140;
-    const noteTime = (60 / bpm) * 0.5;
-
-    // Музыка запустится после первого нажатия клавиши
-    this.input.keyboard.once('keydown', () => {
-    const ctx = new (window.AudioContext || window.webkitAudioContext)();
-    this.audioCtx = ctx;
-
-    this.musicTimer = this.time.addEvent({
-        delay: noteTime * 1000,
-        loop: true,
-        callback: () => {
-        const freq = notes[noteIndex % notes.length];
-        if (freq > 0) {
-            const osc = ctx.createOscillator();
-            const gain = ctx.createGain();
-            osc.connect(gain);
-            gain.connect(ctx.destination);
-            osc.type = 'square';
-            osc.frequency.value = freq;
-            gain.gain.setValueAtTime(0.08, ctx.currentTime);
-            gain.gain.linearRampToValueAtTime(0, ctx.currentTime + noteTime * 0.8);
-            osc.start(ctx.currentTime);
-            osc.stop(ctx.currentTime + noteTime);
-        }
-        noteIndex++;
-        }
-    });
-    });
-
-  // Жизни
-    this.lives = 3;
-    this.livesText = this.add.text(550, 10, '❤️ x3', {
-    fontSize: '14px', fill: '#ff4444'
-    }).setScrollFactor(0);
-
-    // Враг
-  // Создаём красную текстуру для врага
-    const enemyGraphics = this.make.graphics({ x: 0, y: 0, add: false });
-    enemyGraphics.fillStyle(0xff0000);
-    enemyGraphics.fillRect(0, 0, 28, 28);
-    enemyGraphics.generateTexture('enemy', 28, 28);
-    enemyGraphics.destroy();
-
-    this.enemy = this.physics.add.sprite(400, 300, 'enemy');
-    this.enemy.setCollideWorldBounds(true).setBounce(1, 0);
-    this.enemy.setVelocityX(120);
-    this.enemy.setCollideWorldBounds(true).setBounce(1, 0);
-    this.enemy.setVelocityX(120);
-    this.physics.add.collider(this.enemy, platforms);
-
-    // Столкновение игрока с врагом
-    this.physics.add.overlap(this.player, this.enemy, () => {
+  // Столкновение с врагом
+  this.physics.add.overlap(this.player, this.enemy, () => {
     this.lives--;
     this.livesText.setText('❤️ x' + this.lives);
     this.playSound(100, 50, 0.3, 'sawtooth');
-    // Откидываем игрока назад
     this.player.setVelocityX(this.player.x < this.enemy.x ? -300 : 300);
     this.player.setVelocityY(this.gravityFlipped ? 300 : -300);
-
     if (this.lives <= 0) {
-        this.add.text(220, 160, 'GAME OVER', {
+      this.add.text(220, 160, 'GAME OVER', {
         fontSize: '48px', fill: '#ff0000'
-        });
-        this.physics.pause();
+      });
+      this.physics.pause();
     }
-    });
+  });
 
+  // Управление
+  this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+  this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+  this.keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+  this.keyX = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
+  this.shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
+  this.gravityFlipped = false;
+
+  // Звуки
+  this.playSound = function(freq1, freq2, duration, type = 'square') {
+    if (!this.audioCtx) return;
+    const ctx = this.audioCtx;
+    const osc = ctx.createOscillator();
+    const gain = ctx.createGain();
+    osc.connect(gain);
+    gain.connect(ctx.destination);
+    osc.type = type;
+    osc.frequency.setValueAtTime(freq1, ctx.currentTime);
+    osc.frequency.linearRampToValueAtTime(freq2, ctx.currentTime + duration);
+    gain.gain.setValueAtTime(0.3, ctx.currentTime);
+    gain.gain.linearRampToValueAtTime(0, ctx.currentTime + duration);
+    osc.start(ctx.currentTime);
+    osc.stop(ctx.currentTime + duration);
+  }.bind(this);
+
+  // Подсказка
+  this.add.text(10, 340, 'S D движение  |  E прыжок вверх  |  X прыжок вниз  |  SHIFT гравитация', {
+    fontSize: '10px', fill: '#aaaaaa'
+  }).setScrollFactor(0);
+
+  // Музыка после первого нажатия
+  const notes = [329,329,0,329,0,262,329,0,392,0,0,0,196,0,0,0];
+  let noteIndex = 0;
+  const noteTime = (60 / 140) * 0.5;
+
+  this.input.keyboard.once('keydown', () => {
+    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    this.time.addEvent({
+      delay: noteTime * 1000,
+      loop: true,
+      callback: () => {
+        const freq = notes[noteIndex % notes.length];
+        if (freq > 0) {
+          const ctx = this.audioCtx;
+          const osc = ctx.createOscillator();
+          const gain = ctx.createGain();
+          osc.connect(gain);
+          gain.connect(ctx.destination);
+          osc.type = 'square';
+          osc.frequency.value = freq;
+          gain.gain.setValueAtTime(0.08, ctx.currentTime);
+          gain.gain.linearRampToValueAtTime(0, ctx.currentTime + noteTime * 0.8);
+          osc.start(ctx.currentTime);
+          osc.stop(ctx.currentTime + noteTime);
+        }
+        noteIndex++;
+      }
+    });
+  });
 }
 
 function update() {
+  if (!this.player) return;
   const onGround = this.player.body.blocked.down || this.player.body.blocked.up;
 
   if (this.keyS.isDown) {
     this.player.setVelocityX(-220);
+    this.player.setFlipX(true);
   } else if (this.keyD.isDown) {
     this.player.setVelocityX(220);
+    this.player.setFlipX(false);
   } else {
     this.player.setVelocityX(0);
   }
 
-  // E — прыжок вверх, X — прыжок вниз (при инвертированной гравитации)
   if (this.keyE.isDown && onGround && !this.gravityFlipped) {
     this.player.setVelocityY(-450);
-    new Audio(this.soundJump).play();
+    this.playSound(200, 400, 0.15);
   }
   if (this.keyX.isDown && onGround && this.gravityFlipped) {
     this.player.setVelocityY(450);
-    new Audio(this.soundJump).play();
+    this.playSound(200, 400, 0.15);
   }
 
   if (Phaser.Input.Keyboard.JustDown(this.shiftKey)) {
     this.gravityFlipped = !this.gravityFlipped;
     this.physics.world.gravity.y = this.gravityFlipped ? -600 : 600;
     this.player.setFlipY(this.gravityFlipped);
-    new Audio(this.soundFlip).play();
+    this.playSound(400, 100, 0.25, 'sawtooth');
   }
 }
